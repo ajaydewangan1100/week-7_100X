dc943a7f90582085a056a2e121168de7abdffd1c
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2024-03-12 13:24:23 +0530

    week7 - state management Recoil done

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,55 @@
-
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
+import { countAtom } from "./store/atoms/count";
 
 function App() {
   return (
     <div>
+      <RecoilRoot>
         <Count />
+      </RecoilRoot>
     </div>
-  )
+  );
 }
 
 function Count() {
-  return <div>
-    <CountRenderer />
-    <Buttons />
-  </div>
+  return (
+    <div>
+      <CountRenderer />
+      <Buttons />
+    </div>
+  );
 }
 
-function CountRenderer() {  
-  return <div>
-    <b>
-      {count}
-    </b>
-  </div>
+function CountRenderer() {
+  const count = useRecoilValue(countAtom);
+  return (
+    <div>
+      <b>{count}</b>
+    </div>
+  );
 }
-
 
 function Buttons() {
-  return <div>
-    <button onClick={() => {
-      setCount(count => count + 1)
-    }}>Increase</button>
+  const [count, setCount] = useRecoilState(countAtom);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount((count) => count + 1);
+        }}
+      >
+        Increase
+      </button>
 
-    <button onClick={() => {
-      setCount(count => count - 1)
-    }}>Decrease</button>
-  </div>
+      <button
+        onClick={() => {
+          setCount((count) => count - 1);
+        }}
+      >
+        Decrease
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
