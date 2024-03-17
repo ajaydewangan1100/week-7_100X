27c9d446dc2f68872328d939f7203c9ebb47e072
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2024-03-17 19:39:06 +0530

    Recoil async - done

--- Recoil_async/src/atoms.js
+++ Recoil_async/src/atoms.js
@@ -1,13 +1,21 @@
+import axios from "axios";
 import { atom, selector } from "recoil";
 
 export const notifications = atom({
   key: "networkAtom",
-  default: {
-    network: 0,
-    jobs: 0,
-    messaging: 0,
-    notifications: 0,
-  },
+  default: selector({
+    key: "networkAtomSelector",
+    get: async () => {
+      // for making a flash to see what will show if async take time
+      // await new Promise((a) => setTimeout(a, 5000));
+
+      // actual code to fetch async
+      const res = await axios.get(
+        "https://sum-server.100xdevs.com/notifications"
+      );
+      return res.data;
+    },
+  }),
 });
 
 export const totalNotificationSelector = selector({
