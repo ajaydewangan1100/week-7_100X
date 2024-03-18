320120290a2829218622359e5dc9f272d77d19c8
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2024-03-18 15:20:42 +0530

    selectorFamily - done

---
+++ selector-family/src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css";
+import { RecoilRoot, useRecoilState } from "recoil";
+import { todosAtomFamily } from "./atoms";
+
+function App() {
+  return (
+    <RecoilRoot>
+      <Todo id={1} />
+      <Todo id={2} />
+      <Todo id={2} />
+      <Todo id={2} />
+      <Todo id={3} />
+    </RecoilRoot>
+  );
+}
+
+function Todo({ id }) {
+  const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
+
+  return (
+    <h4>
+      {todo.title}
+      <br />
+      {todo.description}
+    </h4>
+  );
+}
+
+export default App;
